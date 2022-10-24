9ad80bd449b3c6be7246dc2e6e8ca41572a9ed04
Jonathan <jonathanborbon222@gmail.com>
2022-10-24 20:02:21 +0800

    adding admin folder with dash board controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::prefix('admin')->group(function(){
+   Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 });
