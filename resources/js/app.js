88e7b03942cc21678c2c1a50a95eb30fe305f355
Jonathan <jonathanborbon222@gmail.com>
2022-10-25 13:24:24 +0800

    editing the layouts

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,5 @@ import './bootstrap';
 
 import '../sass/app.scss';
 import * as bootstrap from 'bootstrap';
+
+require('./bootstrap');
--- resources/views/pagelayout/app.blade.php
+++ resources/views/pagelayout/app.blade.php
@@ -7,7 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link  href="{{asset('build/assets/app.css')}}" rel="stylesheet" >
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
 <body>
     
 
@@ -32,18 +33,16 @@
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Dropdown link
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
+             
             </li>
           </ul>
         </div>
       </nav>
-
-<div>
-    @yield('pagecontent')
-</div>
+     
+      
+  <div>
+      @yield('pagecontent')
+  </div>
 </body>
+<script src="{{asset('build/assets/app.js')}}" defer></script>
 </html>
