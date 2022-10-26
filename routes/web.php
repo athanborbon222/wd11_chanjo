c1a8986eea263103ce92bf8ff0370fffe63b8a01
Jonathan <jonathanborbon222@gmail.com>
2022-10-26 18:17:46 +0800

    Add AdminMiddleware.php

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::prefix('admin')->group(function(){
+Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function(){
    Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 });
+ 
